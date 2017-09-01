9d8fa000d80c3d712e3111b377b7c68a906b04ff
douduck08 <lu10082000@gmail.com>
2017-09-01 20:04:00 +0800

    modify easing

--- Assets/Scripts/Math/Easing.cs
+++ Assets/Scripts/Math/Easing.cs
@@ -3,38 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace DouduckGame.Math {
-    public enum EaseType {
+    public enum Easing {
         Linear,
+
         EaseInQuad,
         EaseOutQuad,
         EaseInOutQuad,
+        EaseOutInQuad,
+
         EaseInCubic,
         EaseOutCubic,
         EaseInOutCubic,
+        EaseOutInCubic,
+
         EaseInQuart,
         EaseOutQuart,
         EaseInOutQuart,
+        EaseOutInQuart,
+
         EaseInQuint,
         EaseOutQuint,
         EaseInOutQuint,
+        EaseOutInQuint,
+
         EaseInSine,
         EaseOutSine,
         EaseInOutSine,
+        EaseOutInSine,
+
         EaseInExpo,
         EaseOutExpo,
         EaseInOutExpo,
+        EaseOutInExpo,
+
         EaseInCirc,
         EaseOutCirc,
         EaseInOutCirc,
+        EaseOutInCirc,
+
         EaseInBack,
         EaseOutBack,
         EaseInOutBack,
+        EaseOutInBack,
+
         EaseInElastic,
         EaseOutElastic,
         EaseInOutElastic,
+        EaseOutInElastic,
+
         EaseInBounce,
         EaseOutBounce,
-        EaseInOutBounce
+        EaseInOutBounce,
+        EaseOutInBounce
     }
 }
 
---
+++ Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DouduckGame.Math;
+
+public class Test : MonoBehaviour {
+
+    [SerializeField]
+    private int m_pointNumber;
+    [SerializeField]
+    private int m_scale;
+
+    void OnDrawGizmos () {
+        float deltaX_ = 1f / m_pointNumber;
+        for (int i = 0; i < m_pointNumber; i++) {
+            float x1 = deltaX_ * i;
+            float x2 = deltaX_ * (i + 1);
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine (new Vector2 (x1, EasingEquation.EaseInBounce (x1)) * m_scale, new Vector2 (x2, EasingEquation.EaseInBounce (x2)) * m_scale);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine (new Vector2 (x1, EasingEquation.EaseOutBounce (x1)) * m_scale, new Vector2 (x2, EasingEquation.EaseOutBounce (x2)) * m_scale);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine (new Vector2 (x1, EasingEquation.EaseInOutBounce (x1)) * m_scale, new Vector2 (x2, EasingEquation.EaseInOutBounce (x2)) * m_scale);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine (new Vector2 (x1, EasingEquation.EaseOutInBounce (x1)) * m_scale, new Vector2 (x2, EasingEquation.EaseOutInBounce (x2)) * m_scale);
+        }
+    }
+}
