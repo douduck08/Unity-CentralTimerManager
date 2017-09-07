f2ecd64cce457a26212837c17e39e2deb5e20f73
douduck08 <lu10082000@gmail.com>
2017-09-08 01:22:29 +0800

    find hope to get general instance of property

--- Assets/EasingTest.cs
+++ Assets/EasingTest.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using DouduckGame.Math;
+using UnityEngine;
 
-public class Test : MonoBehaviour {
+public class EasingTest : MonoBehaviour {
 
-	public Easing m_easingEquation = new Easing(EasingType.Linear);
-    public EasingCubicBezier m_cubicBezier = new EasingCubicBezier (new Vector2 (0, 1), new Vector2 (1, 0));
+    [System.Serializable]
+    public struct EasingData {
+        public GameObject go;
+        public Easing easing;
+    }
 
-    [SerializeField]
-    private int m_pointNumber;
-    [SerializeField]
-    private int m_scale;
+    public Easing m_easing;
+    public EasingData m_inStuct;
+    public List<Easing> m_inList;
+    public List<EasingData> m_inStuctList;
 
+    public float height = 10f;
+    public float durability = 2f;
+    public float m_time = 0f;
+
+    private void Update () {
+        m_time += Time.deltaTime;
+        for (int i = 0; i < m_inStuctList.Count; i++) {
+            if (m_inStuctList[i].go != null) {
+                float x = m_inStuctList[i].go.transform.position.x;
+                m_inStuctList[i].go.transform.position = new Vector2 (0, m_inStuctList[i].easing.Ease (m_time / durability) * height) * 10f;
+            }
+        }
+        if (m_time > durability) {
+            m_time -= durability;
+        }
+    }
+
+    //[SerializeField]
+    //private int m_pointNumber;
+    //[SerializeField]
+    //private int m_scale;
     //void OnDrawGizmos () {
     //    float deltaX_ = 1f / m_pointNumber;
     //    for (int i = 0; i < m_pointNumber; i++) {
@@ -28,16 +52,4 @@ public class Test : MonoBehaviour {
     //        Gizmos.DrawLine (new Vector2 (x1, Easing.EaseOutInBounce (x1)) * m_scale, new Vector2 (x2, Easing.EaseOutInBounce (x2)) * m_scale);
     //    }
     //}
-
-    public GameObject go1;
-    public GameObject go2;
-    public float time = 0f;
-    private void Update () {
-        go1.transform.position = new Vector2 (0, m_easingEquation.Ease (time / 3f)) * 10f;
-        go2.transform.position = new Vector2 (0.5f, m_cubicBezier.Ease (time / 3f)) * 10f;
-        time += Time.deltaTime;
-        if (time > 3f) {
-            time -= 3f;
-        }
-    }
 }
--- Assets/Scripts/Math/Editor/EasingDrawer.cs
+++ Assets/Scripts/Math/Editor/EasingDrawer.cs
@@ -1,75 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-using UnityEngine;
-using UnityEditor;
+using System.Linq;
+using System;
 using DouduckGame.Math;
+using UnityEditor;
+using UnityEngine;
 
 [CustomPropertyDrawer (typeof (Easing))]
 public class EasingDrawer : PropertyDrawer {
 
-	private readonly float curveFieldHeight = 150f;
-	private readonly int pointNumber = 50;
+    private readonly float curveFieldHeight = 150f;
+    private readonly int pointNumber = 50;
 
     private Easing m_target = null;
     private Easing GetTarget (SerializedProperty property) {
         if (m_target == null) {
-            var obj = this.fieldInfo.GetValue (property.serializedObject.targetObject);
-            m_target = (Easing)obj;
+            m_target = SerializedPropertyExtensions.GetNestedObject<Easing> (property.propertyPath, property.serializedObject.targetObject);
+            // m_target = GetValue<Easing> (property);
         }
+        Debug.Log(m_target);
         return m_target;
     }
 
-	public override float GetPropertyHeight (SerializedProperty property, GUIContent label) {
+    private T GetValue<T> (SerializedProperty property) where T : class {
+        FieldInfo field = property.serializedObject.targetObject.GetType ().GetField (property.propertyPath);
+        if (field != null) {
+            Debug.Log ("Sucess: " + property.propertyPath);
+            var value = field.GetValue (property.serializedObject.targetObject);
+            if (value.GetType ().IsArray) {
+                var index = Convert.ToInt32 (new string (property.propertyPath.Where (c => char.IsDigit (c)).ToArray ()));
+                return ((T[]) value)[index];
+            } else {
+                return (T) value;
+            }
+        }
+        Debug.Log ("Failed: " + property.propertyPath);
+        return null;
+    }
+
+    public override float GetPropertyHeight (SerializedProperty property, GUIContent label) {
         float baseHeight_ = base.GetPropertyHeight (property, label);
         return property.isExpanded ? curveFieldHeight + baseHeight_ : baseHeight_;
     }
 
-	public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
-		label = EditorGUI.BeginProperty (position, label, property);
+    public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
+        label = EditorGUI.BeginProperty (position, label, property);
 
-		EditorGUI.BeginChangeCheck ();
-		EditorGUI.PropertyField (position, property.FindPropertyRelative ("m_settedType"), new GUIContent ("Easing Type"));
-		if (EditorGUI.EndChangeCheck ()) {
-            GetTarget(property).easingType = (EasingType)(property.FindPropertyRelative ("m_settedType").enumValueIndex);
+        EditorGUI.BeginChangeCheck ();
+        EditorGUI.PropertyField (position, property.FindPropertyRelative ("m_settedType"), label);
+        if (EditorGUI.EndChangeCheck ()) {
+            GetTarget (property);
+            //GetTarget(property).easingType = (EasingType)(property.FindPropertyRelative ("m_settedType").enumValueIndex);
         }
 
         property.isExpanded = EditorGUI.Foldout (position, property.isExpanded, label);
-		if (property.isExpanded) {
-			Rect contentPosition = EditorGUI.IndentedRect (position);
-			contentPosition.y += 18f;
-			contentPosition.height = curveFieldHeight;
-			DrawCurveField (contentPosition, property);
-		}
+        if (property.isExpanded) {
+            Rect contentPosition = EditorGUI.IndentedRect (position);
+            contentPosition.y += 18f;
+            contentPosition.height = curveFieldHeight;
+            //    DrawCurveField (contentPosition, property);
+        }
 
-		EditorGUI.EndProperty();
-	}
+        EditorGUI.EndProperty ();
+    }
 
-	private void DrawCurveField (Rect position, SerializedProperty property) {
-		if (m_target == null) {
+    private void DrawCurveField (Rect position, SerializedProperty property) {
+        if (m_target == null) {
             GetTarget (property);
         }
 
-		position.x = (position.width - curveFieldHeight) / 2f;
-		position.width = curveFieldHeight;
-		EditorGUI.DrawRect (position, Color.gray);
+        position.x = (position.width - curveFieldHeight) / 2f;
+        position.width = curveFieldHeight;
+        EditorGUI.DrawRect (position, Color.gray);
 
-		float deltaX_ = 1f / pointNumber;
-		for (int i = 0; i < pointNumber; i++) {
-			float x1 = deltaX_ * i;
-			float x2 = deltaX_ * (i + 1);
-			Vector2 point1 = new Vector2 (x1, m_target.Ease (x1));
-			Vector2 point2 = new Vector2 (x2, m_target.Ease (x2));
+        float deltaX_ = 1f / pointNumber;
+        for (int i = 0; i < pointNumber; i++) {
+            float x1 = deltaX_ * i;
+            float x2 = deltaX_ * (i + 1);
+            Vector2 point1 = new Vector2 (x1, m_target.Ease (x1));
+            Vector2 point2 = new Vector2 (x2, m_target.Ease (x2));
 
-			point1 *= curveFieldHeight;
-			point1.x += position.x;
-			point1.y = position.y + curveFieldHeight - point1.y;
-			point2 *= curveFieldHeight;
-			point2.x += position.x;
-			point2.y = position.y + curveFieldHeight - point2.y;
+            point1 *= curveFieldHeight;
+            point1.x += position.x;
+            point1.y = position.y + curveFieldHeight - point1.y;
+            point2 *= curveFieldHeight;
+            point2.x += position.x;
+            point2.y = position.y + curveFieldHeight - point2.y;
 
             Handles.color = Color.red;
             Handles.DrawLine (point1, point2);
-		}
-	}
+        }
+    }
 }
